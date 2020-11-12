b550dec692d931eb134970efb9db4b4fe6bcb70d
Manten Devillé <manten.d@hotmail.com>
2020-11-12 17:03:39 +0100

    add if else for hour based greeting

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -11,7 +11,11 @@
 
 (function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    if(new Date ().getHours() < 18) {
+document.getElementById("target").innerHTML = "Hello";
+    } else {
+        document.getElementById("target").innerHTML = "Good evening";
+    }
 
     // your code here
 
