ad32aad0f984f18fd42482c749c67f4ab40f563c
Manten Devillé <manten.d@hotmail.com>
2020-11-12 09:59:00 +0100

    use switch function for calculator 2

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -13,7 +13,24 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     const performOperation = operation => {
-        // perform the operation
+        let one = Number(document.getElementById("op-one").value);
+        let two = Number(document.getElementById("op-two").value);
+
+        switch (operation) {
+            case "addition":
+                alert(one + two);
+                break;
+            case "substraction":
+                alert(one - two);
+                break;
+            case "multiplication":
+                alert(one * two);
+                break;
+            case "division":
+                alert(one / two);
+                break;
+
+        }
     };
 
     Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
