e1461d4d5868ae7c22adad417938ad98a7e74f01
Manten Devillé <manten.d@hotmail.com>
2020-11-12 12:43:32 +0100

    add i to fizzbuzz for clarity

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -13,11 +13,11 @@
     let i;
     for(i = 1; i <= 100; i++) {
         if(i % 5 == 0 && i % 3 == 0) { 
-            console.log("fizzbuzz");
+            console.log(i + "fizzbuzz");
         } else if (i % 5 == 0) {
-            console.log("buzz");
+            console.log(i + "buzz");
         } else if (i % 3 == 0) {
-            console.log("fizz");
+            console.log(i + "fizz");
         } else {
             console.log(i);
         }
