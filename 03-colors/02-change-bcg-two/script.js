69e68381cc96074b1f52761911dd239ee189e5e8
Manten Devillé <manten.d@hotmail.com>
2020-11-12 16:12:00 +0100

    color 2 , delete 1st function, fill in with getelementbyid.

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -9,8 +9,11 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
 
-})();
+    document.getElementById("run").addEventListener("click", function() {
+
+        let bgc = document.getElementById("color").value;
+
+        document.body.style.backgroundColor = bgc;
+    })();
