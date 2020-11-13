17501eb68fe7f970f39a0ef4be3f6139e216e061
Manten Devillé <manten.d@hotmail.com>
2020-11-13 13:21:10 +0100

    add code to age by select 0403

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,8 +9,32 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+document.getElementById("run").addEventListener("click", function () {
 
-    // your code here
+    function getAge(birth) {
+        let day = document.getElementById("dob-day").value;
+        let month = document.getElementById("dob-month").value;
+        let year = document.getElementById("dob-year").value;
+        const dob = day + "," + month + "," + year;
 
-})();
+        let today = new Date();
+        let d = today.getDay();
+        let m = today.getMonth();
+        let y = today.getFullYear();
+
+        if (m == month && d >= day) return parseInt(y - year);
+        if (m == month && d < day) return parseInt(y - year - 1);
+        if (m > month) return parseInt(y - year);
+        if (m < month) return parseInt(y - year - 1);
+
+    }
+
+    let age = getAge("dob");
+    alert("Your birthday is " + dob + " and you are " + age + " years old.");
+
+
+
+
+
+
+});
