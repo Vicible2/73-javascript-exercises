54fd6680d12a2d51dd8edab7fed5f06a5a8d168a
Manten Devillé <manten.d@hotmail.com>
2020-11-13 13:44:54 +0100

    alert still not working with 2 vars

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,7 +9,7 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-document.getElementById("run").addEventListener("click", function () {
+ document.getElementById("run").addEventListener("click", function () {
 
     function getAge(birth) {
         let day = document.getElementById("dob-day").value;
@@ -22,15 +22,19 @@ document.getElementById("run").addEventListener("click", function () {
         let m = today.getMonth();
         let y = today.getFullYear();
 
+        console.log(dob);
+
         if (m == month && d >= day) return parseInt(y - year);
         if (m == month && d < day) return parseInt(y - year - 1);
         if (m > month) return parseInt(y - year);
         if (m < month) return parseInt(y - year - 1);
 
     }
-
+    const bday = day + "," + month + "," + year;
     let age = getAge("dob");
-    alert("Your birthday is " + dob + " and you are " + age + " years old.");
+    alert("Your birthday is " + bday + " and you are " + age + " years old.");
+
+
 
 
 
